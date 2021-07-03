ee9dd2432969fdf217ec8497ccd2e16404aed409
H4R1B0 <qkrguswns25@naver.com>
2021-07-03 22:17:10 +0900

    20210702 유닛 추가

---
+++ Assets/Scripts/Battle/Attack/WrightWeapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrightWeapon : Attack
+{
+    private void Awake()
+    {
+        this.transform.position = new Vector3(this.transform.position.x - 0.5f, this.transform.position.y - 1.1f, this.transform.position.z);
+    }
+    void Update()
+    {
+        transform.Translate(vec3dir * Time.deltaTime * moveSpeed);
+    }
+}
