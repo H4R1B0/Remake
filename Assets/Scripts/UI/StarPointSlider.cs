f136cc32111bba9f58a20eacb420f6d11a41b2c6
H4R1B0 <qkrguswns25@naver.com>
2021-08-13 18:36:34 +0900

    20210813 스타포인트 일정부분마다 수정 지급 변경

--- Assets/Scripts/UI/StarPointSlider.cs
+++ Assets/Scripts/UI/StarPointSlider.cs
@@ -19,7 +19,15 @@ public class StarPointSlider : MonoBehaviour
         //플레이어의 스타포인트가 10개 이상 있는경우
         if (Player.instance.StarPoint >= 10)
         {
-            Player.instance.Crystal += 100; //크리스탈 100증가
+            //플레이어의 스타포인트레벨이 5보다 작으면 100(+50)만큼 수정 지급 
+            if (Player.instance.StarPointLevel < 5)
+            {
+                Player.instance.Crystal += 100 + (Player.instance.StarPointLevel - 1) * 50;
+            }
+            else
+            {
+                Player.instance.Crystal += 300;
+            }
             Player.instance.StarPoint -= 10;
         }
     }
