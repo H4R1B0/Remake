13e4388f5ab437321a7a8978f473c0dda9322599
H4R1B0 <qkrguswns25@naver.com>
2021-08-13 20:52:01 +0900

    20210813 스타포인트에서 수정으로 전환 제한량 삭제

--- Assets/Scripts/UI/StarPointSlider.cs
+++ Assets/Scripts/UI/StarPointSlider.cs
@@ -19,15 +19,8 @@ public class StarPointSlider : MonoBehaviour
         //플레이어의 스타포인트가 10개 이상 있는경우
         if (Player.instance.StarPoint >= 10)
         {
-            //플레이어의 스타포인트레벨이 5보다 작으면 100(+50)만큼 수정 지급 
-            if (Player.instance.StarPointLevel < 5)
-            {
-                Player.instance.Crystal += 100 + (Player.instance.StarPointLevel - 1) * 50;
-            }
-            else
-            {
-                Player.instance.Crystal += 300;
-            }
+            //플레이어의 스타포인트 레벨에 따라 100(+50)만큼 수정 지급
+            Player.instance.Crystal += 100 + (Player.instance.StarPointLevel - 1) * 50;
             Player.instance.StarPoint -= 10;
         }
     }
