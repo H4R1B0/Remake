a16c52481fd4a2eb5ce03147142c7008eadff353
H4R1B0 <qkrguswns25@naver.com>
2021-08-16 16:53:23 +0900

    20210816 유닛 소환 추가 가격 텍스트 추가

--- Assets/Scripts/UI/UnitAddButton.cs
+++ Assets/Scripts/UI/UnitAddButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UnitAddButton : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class UnitAddButton : MonoBehaviour
     void Start()
     {
         player = Player.instance;
+
+        this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = player.CallUnitCountAddPrice.ToString();
+
         this.GetComponent<Button>().onClick.AddListener(player.CallUnitCountAdd);
         if (player.CallUnitCountMax >= 7)
         {
