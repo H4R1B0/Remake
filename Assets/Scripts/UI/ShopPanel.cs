98b8719a06821634dd4d9042f9a2063f995aaa90
H4R1B0 <qkrguswns25@naver.com>
2021-08-25 21:30:01 +0900

    20210825 상점에 유닛 구매 추가

---
+++ Assets/Scripts/UI/ShopPanel.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ShopPanel : MonoBehaviour
+{
+    public List<GameObject> PurchaseUnitCards; //구매할 유닛카드 리스트
+    public List<GameObject> UnitPurchaseButtons; //유닛 구매 버튼 리스트
+
+    private Player player; //플레이어
+
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+        player = Player.instance;
+
+        //상점에 있는 유닛카드가 플레이어가 갖고 있다면 비활성화
+        for (int i=0;i< PurchaseUnitCards.Count;i++)
+        {
+            if (player.UnitCards.Contains(PurchaseUnitCards[i]))
+            {
+                UnitPurchaseButtons[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "구매 완료";
+                UnitPurchaseButtons[i].GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                UnitPurchaseButtons[i].GetComponent<Button>().interactable = true;
+            }
+        }
+    }
+
+    //유닛카드 구매
+    public void PurchaseUnitCard()
+    {
+        GameObject clickObject = EventSystem.current.currentSelectedGameObject;
+        //Debug.Log(clickObject);
+        Debug.Log(UnitPurchaseButtons.IndexOf(clickObject));
+        int index = UnitPurchaseButtons.IndexOf(clickObject);
+        if(index>=0 && index <= 5)
+        {
+            player.Coin -= 1000;
+            player.UnitCards.Add(PurchaseUnitCards[index]);
+
+            clickObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "구매 완료";
+            clickObject.GetComponent<Button>().interactable = false;
+        }
+        else if (index >= 6 && index <= 10)
+        {
+            player.Coin -= 2000;
+            player.UnitCards.Add(PurchaseUnitCards[index]);
+
+            clickObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "구매 완료";
+            clickObject.GetComponent<Button>().interactable = false;
+        }
+        else if (index >= 11 && index <= 13)
+        {
+            player.Coin -= 3000;
+            player.UnitCards.Add(PurchaseUnitCards[index]);
+
+            clickObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "구매 완료";
+            clickObject.GetComponent<Button>().interactable = false;
+        }
+    }
+}
